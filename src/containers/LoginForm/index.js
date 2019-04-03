79a3b0530f9ba02c08a4c00cec7a9f01601137a8
Chebotayev Pavel <pavel.chebotayev@gmail.com>
2019-04-03 11:49:24 +0300

    fix bug with login and registration forms

--- src/containers/LoginForm/index.js
+++ src/containers/LoginForm/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Form, Field } from 'react-final-form'
 import { loginActions } from '../../redux/actions';
 import { connect } from 'react-redux';
-import TextField from '@material-ui/core/TextField';
+import { TextField } from 'final-form-material-ui';
 import Button from '@material-ui/core/Button';
 
 //selector
@@ -18,30 +18,29 @@ const LoginForm = ({ loginProcessing, fetching }) => {
     <>
       <Form
         onSubmit={onSubmit}
-        render={({ handleSubmit }) => (
-          <div className='form-wrapper'>
-            <form className='auth-form' onSubmit={handleSubmit}>
-              <div className='auth-form--field'>
-                <Field
-                  name='email'
-                  component={TextField}
-                  label='Email'
-                  type='email'
-                  variant='outlined'
-                />
-              </div>
-              <div className='auth-form--field'>
-                <Field
-                  name='password'
-                  component={TextField}
-                  label='Password'
-                  type='password'
-                  variant='outlined'
-                />
-              </div>
-              <Button type='submit' variant="contained" color="primary">Submit</Button>
-            </form>
-          </div>
+        render={({ handleSubmit }) => (<div className='form-wrapper'>
+          <form className='auth-form' onSubmit={handleSubmit}>
+            <div className='auth-form--field'>
+              <Field
+                name='email'
+                component={TextField}
+                label='Email'
+                type='text'
+                variant='outlined'
+              />
+            </div>
+            <div className='auth-form--field'>
+              <Field
+                name='password'
+                component={TextField}
+                label='Password'
+                type='password'
+                variant='outlined'
+              />
+            </div>
+            <Button type='submit' variant="contained" color="primary">Submit</Button>
+          </form>
+        </div>
         )} />
       {fetching && <Loader />}
     </>
